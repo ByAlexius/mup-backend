e9d0228c9828ece43d2c168edc6a239932f41166
ByAlexius <alexander@rziha.at>
2025-03-09 23:30:13 +0100

    chore: init

---
+++ src/main/java/at/rziha/mup/MupBackendApplication.java
@@ -0,0 +1,13 @@
+package at.rziha.mup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MupBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/rziha/mup/MupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.rziha.mup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MupBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
